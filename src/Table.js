477017513528873de2da99e46f5770738bc27618
Akash <akash.javali@gmail.com>
2018-08-11 02:09:57 +0530

    clean

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Page, Text, View, Document} from '@react-pdf/react-pdf';
+import { Page, Text, View, Document} from '@react-pdf/react-pdf';
 
 class Table extends Component {
   state={
@@ -84,26 +84,50 @@ class Table extends Component {
         return (
             <div className='container'>
             
-            <table >
-              <tr>
-                <th>FN</th>
-                <th>LN</th>
-                <th>Place</th>
-              </tr>
-              <tr>
-                <td>A</td>
-                <td>J</td>
-                <td>M</td>
-              </tr>
-              <tr>
-                <td>N</td>
-                <td>L</td>
-                <td>P</td>
-              </tr> 
-              
-              
+            <Document>
+            <Page>
 
-            </table>
+              <View style={{ display: "flex", flexDirection: "column", flexBasis: 150,margin: "20px"}}>
+              <View style={{flexDirection: 'row'}}>
+              <View style={{ flex: 1, height: 50, borderWidth: 1, justifyContent: "center", color: 'red'}} >
+              <Text style={{ textAlign: "center" }}>{"Id"}</Text>
+                </View>
+             {
+               this.state.columnData.map((val,key) => {
+                 return(
+                  <View style={{ flex: 1, height: 50, borderWidth: 1, justifyContent: "center", color: 'red'}} key={key} >
+                      <Text style={{ textAlign: "center" }}>{val.name}</Text>
+                  </View>
+                 )
+
+               })
+             }
+             </View>
+             
+             {
+                  this.state.tableData.map((val,key) => {
+                 return(
+                  <View style={{flexDirection: "row"}} key={key} >
+                    <View style={{ flex: 1, height: 50, borderWidth: 1, justifyContent: "center", color: 'red' }} key={key} >
+                            <Text style={{ textAlign: "center" }}>{key + 1}</Text>
+                    </View>
+                  {
+                    this.state.columnData.map((data, index) => {
+                      return(
+                        <View style={{ flex: 1, height: 50, borderWidth: 1, justifyContent: "center", color: 'red' }} key={key} >
+                          <Text style={{ textAlign: "center" }}>{val[data.key]}</Text>
+                        </View>
+                      )
+                    })
+                  }
+                   </View>
+                 )
+              })
+             }
+            </View>
+
+            </Page>
+             </Document>
 
              </div>
           );
